3542d98c3f49a5f5ae73e42acf3aa77ad8c446cc
kesompochy <kesompochy@gmail.com>
2022-03-03 07:48:12 +0900

    wip あそんでる

--- src/loader/loader.ts
+++ src/loader/loader.ts
@@ -2,7 +2,6 @@ import Master from '../app/narasmaster';
 
 export default class Loader{
     private _resources: Map<string, AudioBuffer> = new Map();
-    private _datas: Map<string, ArrayBuffer> = new Map();
     private _tasks: Array<Promise<ArrayBuffer>> = [];
     private _loadThen: Function = function(){};
     private _cxt: AudioContext = Master.cxt;
@@ -25,9 +24,25 @@ export default class Loader{
             fetch(src).then((res)=>{
                 return res.arrayBuffer();
             }).then((data)=>{
-                this._datas.set(id, data);
                 return this._cxt.decodeAudioData(data);
             }).then((buf)=>{
+
+                const audio = Master.cxt.createBuffer(buf.numberOfChannels, buf.length, buf.sampleRate);
+
+                for(let i=0;i<audio.numberOfChannels;i++){
+                    const data = audio.getChannelData(i);
+                    const bufData = buf.getChannelData(i);
+                    const freq = 100;
+                    const pitch = 2;
+                    for(let j=0;j<data.length/freq;j++){
+                        for(let k=0;k<freq;k++){
+                            data[j*freq + k] = bufData[j*freq + pitch*(k%(freq/pitch))];
+                        }
+                    }
+                }
+
+
+                this._resources.set('processed', audio);
                 this._resources.set(id, buf);
                 resolve(buf);
             })
@@ -37,8 +52,5 @@ export default class Loader{
     }
     getResource(id: string): AudioBuffer | undefined{
         return this._resources.get(id);
-    }
-    getData(id: string): ArrayBuffer | undefined{
-        return this._datas.get(id);
     }
 }
