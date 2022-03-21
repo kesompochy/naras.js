b21843c5c0266e13340ef7c3cd409847f339e059
kesompochy <kesompochy@gmail.com>
2022-03-22 03:39:49 +0900

    typesを追加

---
+++ dist/app/master.d.ts
@@ -0,0 +1,9 @@
+declare global {
+    interface Window {
+        webkitAudioContext: any;
+    }
+}
+export default class Master {
+    static cxt: AudioContext;
+}
+//# sourceMappingURL=master.d.ts.map
